b03bd692ef157192ff1f7d9a71c4739b4635e856
rozzledred <56243125+rozzledred@users.noreply.github.com>
2020-09-02 17:21:13 -0400

    First commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+import os
+THERM = "C:\Users\arnav\Desktop\cpropep\data\thermo.dat"
+PROP = "C:\Users\arnav\Desktop\cpropep\data\propellant.dat"
+
+
+def load_therm():
+    therm = open(THERM)
+
+
+def load_prop():
+    prop = open(PROP)
